6dfd8dbf1f1e1c293dc606dd5f58afa29da9c938
thatblindgeye <thatblindgeye@gmail.com>
2021-11-26 14:51:15 -0500

    Breakout user card to separate component

--- step-3/src/App.js
+++ step-3/src/App.js
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
     return (
-        <main>
+        <main aria-live='polite' aria-atomic='true'>
             <Outlet />
         </main>
     );
