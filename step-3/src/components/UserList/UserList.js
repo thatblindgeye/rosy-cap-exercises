b03211f4f381a4a200362a5b26d3a3b19aa5e9f4
thatblindgeye <thatblindgeye@gmail.com>
2021-11-27 13:09:50 -0500

    Add functionality to fetch user list

--- step-3/src/components/UserList/UserList.js
+++ step-3/src/components/UserList/UserList.js
@@ -1,33 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom'
 import {ReactComponent as InfoIcon} from '../../assets/icons/info.svg'
 import {ReactComponent as PersonIcon} from '../../assets/icons/person.svg'
 import './userList.css'
 
-function UserCard() {
-  
+function UserCard({user}) {
+  const {avatar, first_name, last_name, id} = user;
+
   return (
     <article className='user-card'>
       <div className='user-card__top-container'>
         <div className='user-card__image-container'>
           <img
-            src='https://reqres.in/img/faces/1-image.jpg'
-            alt='George Bluth'
+            src={avatar}
+            alt={`${first_name} ${last_name}`}
             className='user-card__image'
           />
         </div>
-        <Link to='1' className='user-card__details-link'
-        aria-describedby='user-1'>
+        <Link to={`${id}`} className='user-card__details-link'
+        aria-describedby={`user-${id}`}>
           <InfoIcon className='user-card__icon' aria-hidden='true' />
           See details
         </Link>
       </div>
-      <h2 id='user-1' className='user-card__name'>
-      George Bluth
+      <h2 id={`user-${id}`} className='user-card__name'>
+      {first_name} {last_name}
       </h2>
       <div className='user-card__id-container'>
         <PersonIcon className='user-card__icon' aria-hidden='true' />
-        <span className='user-card__id'>ID: 1</span>
+        <span className='user-card__id'>ID: {id}</span>
       </div>
     </article>
   )
@@ -35,12 +36,54 @@ function UserCard() {
 
 
 export default function UserList() {
-  return (
-    <>
-      <h1>Users</h1>
-      <div className='users-container'>
-        <UserCard />
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [fetchError, setFetchError] = useState('');
+
+  const fetchUserList = async (url) => {
+    try {
+      const response = await fetch(url, {
+        mode: 'cors'
+      });
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const responseData = await response.json();
+      setIsLoading(false);
+      setUsers(responseData.data);
+    } catch (error) {
+      setFetchError('Unable to fetch user list. Please refresh the page to try again.');
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchUserList('https://reqres.in/api/users');
+  }, [])
+
+  if (fetchError) {
+    return (
+      <div className='error-container'>
+        <h1>{fetchError}</h1>
       </div>
-    </>
-  );
+    )
+  } else if (isLoading) {
+    return (
+      <div className='loading-container'>
+        <div className='loading-icon' aria-label='Loading'></div>
+      </div>
+    )
+  } else {
+    return (
+      <>
+        <h1>Users</h1>
+        <div className='users-container'>
+          {users.map(user => {
+            return <UserCard user={user} />
+          })}
+        </div>
+      </>
+    );
+  }
 }
